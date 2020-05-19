bc4c1ee1054d1ec63ea9f1a10cb011d2d4eb8b79
User <User@211-11>
2020-05-19 13:47:19 +0900

    두번째 테스트 - 자바연습프로젝트 업로드

--- 20200519/src/magicSquare2/MagicSquareMain2.java
+++ 20200519/src/magicSquare2/MagicSquareMain2.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class MagicSquareMain2 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("3이상의 홀수를 입력하세요.");
+		System.out.println("1초과의 홀수를 입력하세요.");
 		int num = input.nextInt();
 		input.close();
 		
