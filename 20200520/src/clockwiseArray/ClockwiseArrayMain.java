97da1236593a2aa792be04e84573f07a34ebaa7f
minji-ing <kwakmj9010@gmail.com>
2020-05-20 10:13:40 +0900

    두번째 - 변수 수정

--- 20200520/src/clockwiseArray/ClockwiseArrayMain.java
+++ 20200520/src/clockwiseArray/ClockwiseArrayMain.java
@@ -6,10 +6,10 @@ public class ClockwiseArrayMain {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("2이상의 자연수를 입력해주세요.");
-		int num = input.nextInt();
+		int n = input.nextInt();
 		input.close();
 		
-		ClockwiseArray arr = new ClockwiseArray(num);
+		ClockwiseArray arr = new ClockwiseArray(n);
 		
 		arr.make();
 		arr.print();
