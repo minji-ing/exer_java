c54a82e12ad340a403a370098d0136187b4d04f9
minji-ing <kwakmj9010@gmail.com>
2020-05-22 16:04:31 +0900

    네번째 프로젝트 - 자바연습프로젝트 업로드

---
+++ 20200522/src/sumAliquot/SumAliquot.java
@@ -0,0 +1,14 @@
+package sumAliquot;
+
+public class SumAliquot {
+	public int sum(int n) {
+		int answer = 0;
+		
+		for(int i = 1; i <= n/2; i++) {
+			if(n%i == 0) {
+				answer += i;
+			}
+		}
+		return answer + n;
+	}
+}
---
+++ 20200522/src/sumAliquot/SumAliquotMain.java
@@ -0,0 +1,9 @@
+package sumAliquot;
+
+public class SumAliquotMain {
+	public static void main(String[] args) {
+		SumAliquot sum = new SumAliquot();
+		
+		System.out.println(sum.sum(12));
+	}
+}
