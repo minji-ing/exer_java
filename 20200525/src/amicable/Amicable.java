b7217679408efb38868e51ec4918ee4c4e8a8247
minji-ing <kwakmj9010@gmail.com>
2020-05-25 10:08:39 +0900

    여섯번째 프로젝트 - 자바연습프로젝트 업로드

---
+++ 20200525/src/amicable/Amicable.java
@@ -0,0 +1,46 @@
+package amicable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Amicable {
+	public int a;
+	public int count = 0;
+	Map<Integer, Integer> match = new HashMap<Integer, Integer>();
+	
+	//자신을 제외한 약수 합 구하기
+	public int sum(int num) {
+		int sum = 0;
+		
+		for(int i = 2; i < num; i++) {
+			if(num%i == 0) {
+				sum = sum + i;
+			}
+		}
+		sum = sum + 1;
+		return sum;
+	}
+	
+	//친화수 확인
+	public Map chk(int a) {
+		this.a = a;
+		for(int i = 1; i <= a; i++) {
+			for(int j = i+1; j <= a; j++) {
+				if(sum(i) == j && sum(j) == i && i != j) {
+					match.put(i, j);
+					count++;
+				}
+			}
+		}
+		return match;
+	}
+	
+	public void print() {
+		System.out.println("1부터 "+a+" 범위 내의 친화수");
+		for(int key : match.keySet()) {
+			int value = match.get(key);
+			System.out.println(key +":"+value);
+		}
+		System.out.println("범위 내 총 친화수의 갯수는 "+count+"개 이다.");
+	}
+}
