32e6d57f66069dcf43d97d4777d6a7ef8c5f3cde
minji-ing <kwakmj9010@gmail.com>
2020-06-06 01:18:47 +0900

    17번째 프로젝트 - 자바연습프로젝트 업로드

---
+++ 20200606/src/diffNum/DiffNumMain.java
@@ -0,0 +1,11 @@
+package diffNum;
+
+public class DiffNumMain {
+	public static void main(String[] args) {
+		int[] arr = {4,4,4,3,3};
+		DiffNum diff = new DiffNum();
+		
+		diff.make(arr);
+		diff.print();
+	}
+}
