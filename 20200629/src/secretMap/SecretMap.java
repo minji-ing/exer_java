dde9031e64814246032424a972ea075ce2158c66
minji-ing <kwakmj9010@gmail.com>
2020-07-01 23:48:06 +0900

    39번째 프로젝트 - 프로그래머스(비밀지도)-answer 수정

--- 20200629/src/secretMap/SecretMap.java
+++ 20200629/src/secretMap/SecretMap.java
@@ -4,16 +4,33 @@ public class SecretMap {
 	public String[] make(int n, int[] arr1, int[] arr2) {
 		String[] answer = new String[n];
 		
-		for(int i = 0; i < n; i++) {
-			String binary1 = Integer.toBinaryString(arr1[i]);
-			String binary2 = Integer.toBinaryString(arr2[i]);
-			
-			for(int j = 0; j < n; j++) {
-				if(binary1.charAt(j) == '1' || binary2.charAt(j) == '1') {
-					answer[i] += "#";
+		for(int i = 0; i < 1; i++) {
+			String binary1 = "";
+			String binary2 = "";
+			int j = 1;
+			while(j < n) {
+				binary1 += Integer.toString(arr1[i]%2);
+				binary2 += Integer.toString(arr2[i]%2);
+				if(j < n-1) {
+					arr1[i] = arr1[i]/2;
+					arr2[i] = arr2[i]/2;
+				} else {
+					binary1 += arr1[i]/2;
+					binary2 += arr2[i]/2;
 				}
-				if(binary1.charAt(j) == '0' && binary2.charAt(j) == '0') {
-					answer[i] += " ";
+				j++;
+			}
+			String revBin1 = new StringBuilder(binary1).reverse().toString();
+			String revBin2 = new StringBuilder(binary2).reverse().toString();
+			System.out.println(revBin1);
+			System.out.println(revBin2);
+			
+			for(int k = 0; k < n; k++) {
+				System.out.println(revBin1.charAt(k));
+				if(revBin1.charAt(k) == '1' || revBin2.charAt(k) == '1') {
+					answer[i] += "#";
+				} else if(revBin1.charAt(k) == '0' && revBin2.charAt(k) == '0') {
+					answer[i] = " ";
 				}
 				System.out.print(answer[i]);
 			}
@@ -28,5 +45,9 @@ public class SecretMap {
 		int[] arr2 = {30,1,21,17,28};
 		
 		String[] answer = map.make(5, arr1, arr2);
+		
+		for(int i = 0; i < answer.length; i++) {
+			System.out.println(answer[i]);
+		}
 	}
 }
