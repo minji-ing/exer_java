8896ec4466b084176e463ab81d5cee12d99989f9
minji-ing <kwakmj9010@gmail.com>
2020-07-04 17:09:48 +0900

    41번째 프로젝트 - 프로그래머스(최댓값과 최솟값)-수정필

--- 20200703/src/maxAndMin/MaxAndMin.java
+++ 20200703/src/maxAndMin/MaxAndMin.java
@@ -1,20 +1,26 @@
 package maxAndMin;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class MaxAndMin {
 	public static void main(String[] args) {
 		String s = "1 2 3 4";
-		String answer = "";
-		int len = s.length();
-		int[] arrS = new int[len/2 + 1];
-		int j = 0;
+		List<Integer> arrS = new ArrayList<Integer>();
 		
-		for(int i = 0; i < arrS.length; i++) {
-			if(i == 0) {
-				arrS[i] = s.charAt(i);
-			} 
-			System.out.print(arrS[i]);
+		for(int i = 0; i < s.length(); i+=2) {
+			arrS.add(Character.getNumericValue(s.charAt(i)));
 		}
+		Collections.sort(arrS);
+		
+		String answer = "";
+		
+		answer = Integer.toString(arrS.get(0));
+		answer += " ";
+		answer += Integer.toString(arrS.get(arrS.size()-1));
+		
+		System.out.println(answer);
 	}
 }
