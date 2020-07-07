09eb10f5e754dbd2f68a8c420845d6ad13fa5490
minji-ing <kwakmj9010@gmail.com>
2020-07-08 00:31:01 +0900

    42번째 프로젝트 - 프로그래머스(행렬의 곱셈)-수정필

--- 20200706/src/multiOfArr/MultiOfArr.java
+++ 20200706/src/multiOfArr/MultiOfArr.java
@@ -1,13 +1,11 @@
 package multiOfArr;
-
+// 행렬의 곱셈 구하기
 public class MultiOfArr {
 	public static void main(String[] args) {
 		int[][] arrA = {{1,4},{3,2},{4,1}};
 		int[][] arrB = {{3,3},{3,3}};
-		int[][] multiArr = new int[arrA.length][arrB[0].length];
-		
-		multiArr[0][0] = arrA[0][0]*arrB[0][0];
-		
+		int[][] multiArr = new int[arrA.length][arrB[0].length]; //arrA*arrB를 담는 arr 정의
+
 		for(int i = 0; i < arrA[0].length; i++) {
 			for(int j = 0; j < arrB.length; j++) {
 				
