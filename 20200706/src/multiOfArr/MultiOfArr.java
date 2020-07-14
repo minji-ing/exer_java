a4c5229609ea998adcd25856115a91fd8206d086
minji-ing <kwakmj9010@gmail.com>
2020-07-15 00:02:23 +0900

    42번째 프로젝트 - 프로그래머스(행렬의 곱셈)

--- 20200706/src/multiOfArr/MultiOfArr.java
+++ 20200706/src/multiOfArr/MultiOfArr.java
@@ -2,20 +2,20 @@ package multiOfArr;
 // 프로그래머스(행렬의 곱셈) 구하기
 public class MultiOfArr {
 	public static void main(String[] args) {
-		int[][] arrA = {{2,3,2},{4,2,4},{3,1,4}};
-		int[][] arrB = {{5,4,3},{2,4,1},{3,1,1}};
+		int[][] arrA = {{1,4},{3,2},{4,1}};
+		int[][] arrB = {{3,3},{3,3}};
 		int[][] multiArr = new int[arrA.length][arrB[0].length]; //arrA*arrB를 담는 arr 정의
-		int i = 0;
+		int i = 0; //arrA 행렬의 row 개수
 		
-		while(i < arrA.length) {
-			for(int z = 0; z < arrA[i].length; z++) {
-				for(int j = 0; j < arrB.length; j++) {
-					multiArr[i][z] += arrA[z][j]*arrB[j][i];
-					System.out.println(arrA[i][z]+","+arrB[j][i]+"/"+multiArr[i][z]+"\t");
+		while(i < arrA.length) { //arrA 행렬의 row 개수만큼 loop
+			for(int z = 0; z < arrA[i].length; z++) { //arrA 행렬의 column 개수만큼 loop
+				for(int j = 0; j < arrB.length; j++) { //arrB 행렬의 row 개수만큼 loop
+					multiArr[i][z] += arrA[i][j]*arrB[j][z];
 				}	
 			}
 			i++;
 		}
+		//곱셈해준 행렬 출력
 		for(int n = 0; n < multiArr.length; n++) {
 			for(int m = 0; m < multiArr[n].length; m++) {
 				System.out.print(multiArr[n][m]+"\t");
