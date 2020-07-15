99ac5cb9d358146cddbd0f7321e5148ad0521b5e
minji-ing <kwakmj9010@gmail.com>
2020-07-16 00:16:28 +0900

    45번째 프로젝트 - 프로그래머스(JadenCase)-수정필

---
+++ 20200715/src/jadenCase/JadenCase.java
@@ -0,0 +1,18 @@
+package jadenCase;
+// 프로그래머스 JadenCase 문자열 만들기
+public class JadenCase {
+	public static String make(String s) {
+		String[] cutS = s.split(" ");
+		String answer = "";
+		
+		for(int i = 0; i < cutS.length; i++) {
+
+		}
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		String s = "3people unFollowed me";
+		
+	}
+}
