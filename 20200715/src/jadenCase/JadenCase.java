2a946d8f644452f9a04e2dbf85b23be158d2338a
minji-ing <kwakmj9010@gmail.com>
2020-07-16 00:45:52 +0900

    45번째 프로젝트 - 프로그래머스(JadenCase)-수정필

--- 20200715/src/jadenCase/JadenCase.java
+++ 20200715/src/jadenCase/JadenCase.java
@@ -6,9 +6,7 @@ public class JadenCase {
 		String answer = "";
 		
 		for(int i = 0; i < cutS.length; i++) {
-			if(cutS[i].charAt(0) < 65 || cutS[i].charAt(0) > 90) {
-				
-			}
+			// string 길이만큼 loop?
 		}
 		return answer;
 	}
