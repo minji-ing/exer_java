e597ce59949c92d422e62b672515dce7e51abde4
minji-ing <kwakmj9010@gmail.com>
2020-07-18 23:48:03 +0900

    44번째 프로젝트 - 프로그래머스(H-Index)-수정필

--- 20200712/src/hIndex/H_Index.java
+++ 20200712/src/hIndex/H_Index.java
@@ -1,15 +1,16 @@
 package hIndex;
+
+import java.util.Arrays;
+
 // 프로그래머스 H_Index
 public class H_Index {
 	public static void main(String[] args) {
 		int[] citations = {3,0,1,6,5};
-		int avg = 0;
-		int sum = 0;
-		for(int i = 0; i < citations.length; i++) {
-			sum += citations[i];
-			avg = sum/citations.length;
-		}
+		int midIdx = (citations.length/2);
 		
-		System.out.println(avg);
+		Arrays.sort(citations);
+		int answer = citations[midIdx];
+		
+		System.out.println(answer);
 	}
 }
