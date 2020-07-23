5abb37da856d24d3f2b4b0626a10f893976bdfd1
minji-ing <kwakmj9010@gmail.com>
2020-07-24 01:04:02 +0900

    49번째 프로젝트 - 프로그래머스(피보나치 수)-수정필

--- 20200723/src/fibonacciNum/FibonacciNum.java
+++ 20200723/src/fibonacciNum/FibonacciNum.java
@@ -1,19 +1,34 @@
 package fibonacciNum;
+
+import java.util.ArrayList;
+import java.util.List;
+
 // 프로그래머스 피보나치 수
 public class FibonacciNum {
 	public static void main(String[] args) {
-		int n = 5;
-		int[] f = new int[5];
+		int n = 31;
+		int answer = 0;
+		List<Integer> f = new ArrayList<Integer>();
+		int[] farr = new int[n+1];
+		farr[0] = 0;
+		farr[1] = 1;
+		f.add(0);
+		f.add(1);
 		
-		f[0] = 0;
-		f[1] = 1;
-		
-		for(int i = 0; i < f.length-2; i++) {
-			f[i+2] = f[i] + f[i+1];
+//		if(n <= 2) {
+//			answer = 1;
+//		} else {
+//			for(int i = 0; i < f.size()-2; i++) {
+//				
+//			}
+//			
+//			long answer = f[n]/1234567;	
+//		}
+	
+		for(int i = 0; i < farr.length-2; i++) {
+			farr[i+2] = farr[i] + farr[i+1];
 		}
-		
-		for(int i = 0; i < f.length; i++) {
-			System.out.print(f[i]+"\t");
-		}
+		answer = farr[n]%1234567;
+		System.out.println(answer);
 	}
 }
