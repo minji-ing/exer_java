c6f31e4a3b631d627132d3c89848cbeb240e5275
minji-ing <kwakmj9010@gmail.com>
2020-07-29 01:17:29 +0900

    50번째 프로젝트 - 프로그래머스(카펫)-수정필

--- 20200728/src/carpetSize/CarpetSize.java
+++ 20200728/src/carpetSize/CarpetSize.java
@@ -2,8 +2,9 @@ package carpetSize;
 // 프로그래머스 카펫 사이즈 찾기
 public class CarpetSize {
 	public static void main(String[] args) {
-		int b = 10;
-		int y = 2;
+		int brown = 10;
+		int yellow = 2;
+		
 		
 	}
 }
