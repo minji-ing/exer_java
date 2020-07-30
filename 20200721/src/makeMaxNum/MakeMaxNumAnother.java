08d4bef240c9067b632844dbffe8cea4e841bb7f
minji-ing <kwakmj9010@gmail.com>
2020-07-31 00:02:16 +0900

    47번째 프로젝트 - 프로그래머스(큰 수 만들기)-수정필

---
+++ 20200721/src/makeMaxNum/MakeMaxNumAnother.java
@@ -0,0 +1,28 @@
+package makeMaxNum;
+
+public class MakeMaxNumAnother {
+	public String solution(String number, int k) {
+        int idx = 0;
+        char max;
+	StringBuilder answer = new StringBuilder();
+
+	if(number.charAt(0) == '0') return "0";
+	for(int i = 0; i < number.length() - k; i++) {
+		max = '0';
+		for(int j = idx; j <= k + i; j++) {
+	        	if(max < number.charAt(j)) {
+	        		max = number.charAt(j); idx = j + 1;
+	        	}
+		}			
+		answer.append(max);
+	}
+        return answer.toString();
+    }
+	
+	public static void main(String[] args) {
+		String num = "4177252841";
+		int k = 4;
+		String answer = MakeMaxNum.make(num, k);
+		System.out.println(answer);
+	}
+}
