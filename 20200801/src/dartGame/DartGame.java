2b1faf41dc5576cfa64e0bdf28e22423573ae5fd
minji-ing <kwakmj9010@gmail.com>
2020-08-02 01:28:36 +0900

    51번째 프로젝트 - 프로그래머스(다트게임)-수정필

--- 20200801/src/dartGame/DartGame.java
+++ 20200801/src/dartGame/DartGame.java
@@ -3,6 +3,26 @@ package dartGame;
 public class DartGame {
 	public int dart(String dartResult) {
 		int answer = 0;
+		char[] cutStr = dartResult.toCharArray();
+		
+		for(int i = 1; i < cutStr.length; i++) {
+			if(cutStr[i] == 'S') {
+				answer += cutStr[i-1];
+			} else if(cutStr[i] == 'D') {
+				answer += cutStr[i-1] * cutStr[i-1];
+			} else if(cutStr[i] == 'T') {
+				answer += cutStr[i-1] * cutStr[i-1] * cutStr[i-1];
+			}
+		}
 		return answer;
+	}
+	
+	public static void main(String[] args) {
+		String dartResult = "1S2D*3T";
+		DartGame dart = new DartGame();
+		
+		int answer = dart.dart(dartResult);
+		
+		System.out.println(answer);
 	}
 }
