b123df17574e01d14c9c0d57fc1be5f707b70ab5
minji-ing <kwakmj9010@gmail.com>
2020-08-05 23:53:18 +0900

    52번째 프로젝트 - 프로그래머스(124의 나라)

--- 20200803/src/countryOf124/CountryOf124.java
+++ 20200803/src/countryOf124/CountryOf124.java
@@ -5,15 +5,7 @@ public class CountryOf124 {
 		int n = 56;
 		int accumulate = 0;
 		int i = 1;
-		
-		while(i > 0) {
-			accumulate += Math.pow(3, i);
-			System.out.println(Math.pow(3, i)+":"+accumulate);
-			if(accumulate >= n) {
-				break;
-			}
-			i++;
-		}
+
 		System.out.println(i);
 	}
 }
