f5afc71049989ba7536eddbac54e8a19a25630e0
minji-ing <kwakmj9010@gmail.com>
2020-08-06 00:17:23 +0900

    52번째 프로젝트 - 프로그래머스(124나라)

--- 20200803/src/countryOf124/CountryOf124.java
+++ 20200803/src/countryOf124/CountryOf124.java
@@ -2,10 +2,21 @@ package countryOf124;
 // 프로그래머스 124나라
 public class CountryOf124 {
 	public static void main(String[] args) {
-		int n = 56;
-
-		int i = 1;
-
-		System.out.println(i);
+		String answer = ""; // 정답 저장 string
+		int n = 20;
+		int div = 0;		// n % 3 저장 변수
+		
+		while(n > 0) {
+			div = n % 3;
+			n /= 3;
+			
+			if(div == 0) {
+				n--;
+				div = 4;
+			}
+			answer = div + answer;
+		}
+		
+		System.out.println(answer);
 	}
 }
