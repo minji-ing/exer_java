99603ffec9796b4e86c54f006618fb3092a0cfb2
minji-ing <kwakmj9010@gmail.com>
2020-08-08 02:42:07 +0900

    51번째 프로젝트 - 프로그래머스(다트게임)-수정필

--- 20200801/src/dartGame/DartGame.java
+++ 20200801/src/dartGame/DartGame.java
@@ -3,21 +3,38 @@ package dartGame;
 public class DartGame {
 	public int dart(String dartResult) {
 		int answer = 0;
+		int[] tmp = new int[3];
+		int idx = 0;
 		char[] cutStr = dartResult.toCharArray();
 		
 		for(int i = 1; i < cutStr.length; i++) {
 			int num = Character.getNumericValue(cutStr[i-1]);
 			
-			if(cutStr[i] == 'S') {
-				answer += num;
-			} else if(cutStr[i] == 'D') {
-				answer += num * num;
-			} else if(cutStr[i] == 'T') {
-				answer += num * num * num;
-			} else if(cutStr[i] == '*') {
-				answer = answer * 2;
+			if(i != 1 && 0 <= num && num >= 10) {
+				idx++;
 			}
+			
+			if(cutStr[i] == 'S') {
+				tmp[idx] = num;
+			} else if(cutStr[i] == 'D') {
+				tmp[idx] = num * num;
+			} else if(cutStr[i] == 'T') {
+				tmp[idx] = num * num * num;
+			} else if(cutStr[i] == '*') {
+				if(idx != 0) {
+					tmp[idx-1] = tmp[idx-1] * 2;
+				}
+				tmp[idx] = tmp[idx] * 2;
+			} else {
+				tmp[idx] = tmp[idx] * (-1);
+			}
+			System.out.println(tmp[idx]);
 		}
+		
+		for(int i = 0; i < tmp.length; i++) {
+			answer += tmp[i];
+		}
+		
 		return answer;
 	}
 	
