6792bf87dea62a6aa095b45d832c2de048aa85df
minji-ing <kwakmj9010@gmail.com>
2020-08-12 23:47:39 +0900

    53번째 프로젝트 - 프로그래머스(실패율)-수정필

--- 20200811/src/failureLate/FailureLate.java
+++ 20200811/src/failureLate/FailureLate.java
@@ -1,5 +1,13 @@
 package failureLate;
 // 프로그래머스 실패율
 public class FailureLate {
-
+	public int[] make(int n, int[] stages) {
+		int[] answer = {};
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		int n = 5;
+		int[] stages = {2,1,2,6,2,4,3,3};
+	}
 }
