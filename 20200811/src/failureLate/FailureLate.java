13ad98ac4526e3638618ccbf1a26fc313518b724
minji-ing <kwakmj9010@gmail.com>
2020-08-13 00:39:28 +0900

    53번째 프로젝트 - 프로그래머스(실패율)-수정필

--- 20200811/src/failureLate/FailureLate.java
+++ 20200811/src/failureLate/FailureLate.java
@@ -1,13 +1,38 @@
 package failureLate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 // 프로그래머스 실패율
 public class FailureLate {
 	public int[] make(int n, int[] stages) {
-		int[] answer = {};
-		return answer;
+		int[] answer = new int[n];
+		List<Integer> idx = new ArrayList<Integer>();
+		String stagesStr = "";
+		Arrays.sort(stages);
+		
+		for(int i = 0; i < stages.length; i++) {
+			stagesStr += stages[i];
+		}
+		System.out.println(stagesStr);
+		for(int i = 2; i <= n+1; i++) {
+			idx.add(stagesStr.indexOf(Integer.toString(i)));
+		}
+		
+		return stages;
 	}
 	
 	public static void main(String[] args) {
 		int n = 5;
 		int[] stages = {2,1,2,6,2,4,3,3};
+		
+		FailureLate late = new FailureLate();
+		
+		int[] answer = late.make(n, stages);
+		
+		for(int i = 0; i < answer.length; i++) {
+			System.out.println(answer[i]);
+		}
 	}
 }
