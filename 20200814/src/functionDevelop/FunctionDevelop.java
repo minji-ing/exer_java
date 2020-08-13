53602a640e26bec60dc66b6531ad2fda0208d27f
minji-ing <kwakmj9010@gmail.com>
2020-08-14 00:52:37 +0900

    54번째 프로젝트 - 프로그래머스(기능 개발)-수정필

---
+++ 20200814/src/functionDevelop/FunctionDevelop.java
@@ -0,0 +1,19 @@
+package functionDevelop;
+// 프로그래머스 기능 개발 (스택/큐)
+public class FunctionDevelop {
+	public int[] make(int[] progress, int[] speed) {
+		int[] answer = {};
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		int[] progress = {93, 30, 55};
+		int[] speed = {1, 30, 5};
+		
+		FunctionDevelop dev = new FunctionDevelop();
+		
+		int[] answer = dev.make(progress, speed);
+		
+		System.out.println(answer);
+	}
+}
