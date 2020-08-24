badd0ef9b8201fb95613743b2a09a875b285292a
minji-ing <kwakmj9010@gmail.com>
2020-08-25 00:48:32 +0900

    54번째 프로젝트 - 프로그래머스(기능 개발)-풀이추가필

--- 20200814/src/functionDevelop/FunctionDevelop.java
+++ 20200814/src/functionDevelop/FunctionDevelop.java
@@ -1,20 +1,42 @@
 package functionDevelop;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 // 프로그래머스 기능 개발 (스택/큐)
 public class FunctionDevelop {
 	public int[] make(int[] progress, int[] speed) {
-		int[] days = new int[progress.length];
-		String count = "";
+		int[] days = new int[progress.length]; // 기능별 배포까지 추가로 필요한 날짜 계산 배열
+		List<Integer> n = new ArrayList<Integer>();
+		int count = 1;
 		
+		// 배포까지 필요한 날짜 계산
 		for(int i = 0; i < progress.length; i++) {
 			days[i] = (int)Math.ceil((double)(100 - progress[i]) / speed[i]);
-			System.out.println(days[i]);
 		}
 		
+		int max = days[0];
+		
+		for(int i = 1; i < days.length; i++) {
+			if(days[i] > max) {
+				n.add(count);
+				max = days[i];
+				count = 1;
+			} else {
+				count++;
+				continue;
+			}
+		}
+		n.add(count);
+		
+		int[] answer = new int[n.size()];
+		
+		for(int i = 0; i < answer.length; i++) {
+			answer[i] = n.get(i);
+		}
 
-		return days;
+		return answer;
 	}
 	
 	public static void main(String[] args) {
