6413ecf757e452c6677148c8059ed0c3846019c0
minji-ing <kwakmj9010@gmail.com>
2020-08-24 23:55:35 +0900

    54번째 프로젝트 - 프로그래머스(기능 개발)-수정필

--- 20200814/src/functionDevelop/FunctionDevelop.java
+++ 20200814/src/functionDevelop/FunctionDevelop.java
@@ -10,33 +10,11 @@ public class FunctionDevelop {
 		
 		for(int i = 0; i < progress.length; i++) {
 			days[i] = (int)Math.ceil((double)(100 - progress[i]) / speed[i]);
-//			System.out.println(days[i]);
+			System.out.println(days[i]);
 		}
 		
-		for(int i = 0; i < days.length-1; i++) {
-			if(days[i] < days[i+1]) {
-				count += "1";
-			} else {
-				if(days.length <= 3) {
-					count += "2";
-				}
-				for(int j = i+2; j < days.length; j++) {
-					if(days[i] < days[j]) {
-						count += Integer.toString(j);
-					} else {
-						count += Integer.toString(days.length - i);
-					}
-				}
-			}
-		}
-		
-		int[] answer = new int[count.length()];
-		
-		for(int i = 0; i < answer.length; i++) {
-			answer[i] = Character.getNumericValue(count.charAt(i));
-			System.out.println(answer[i]);
-		}
-		return answer;
+
+		return days;
 	}
 	
 	public static void main(String[] args) {
@@ -46,10 +24,7 @@ public class FunctionDevelop {
 		FunctionDevelop dev = new FunctionDevelop();
 		
 		int[] answer = dev.make(progress, speed);
-		
-		for(int i = 0; i < answer.length; i++) {
-			System.out.print(answer[i] + "\t");
-		}
-		Arrays.toString(answer);
+
+		System.out.println(Arrays.toString(answer));
 	}
 }
