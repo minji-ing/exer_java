8115ad7774678d1b3b9f89721c44e3acb277624d
minji-ing <kwakmj9010@gmail.com>
2020-08-27 01:17:16 +0900

    54번째 프로젝트 - 프로그래머스(기능 개발)-큐 클래스 공부필

--- 20200814/src/functionDevelop/AnotherFunctionDevelop.java
+++ 20200814/src/functionDevelop/AnotherFunctionDevelop.java
@@ -8,6 +8,13 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class AnotherFunctionDevelop {
 
 	public int[] solution(int[] progresses, int[] speeds) {
+		/* queue class는 선입선출
+		 * 인스턴스 생성을 위해서는 Queue n = new LinkedList() 호출
+		 * 메소드 종류
+		 * - boolean off : 객체 넣기
+		 * - poll : 데이터 꺼내오기. 데이터 없으면 null 반환
+		 * - peek : 큐의 맨 아래 객체를 반환. 큐에서 객체를 삭제하지는 않음
+		 */
         Queue<Integer> q = new ConcurrentLinkedQueue<Integer>();
         
         for(int i = 0; i < progresses.length; i++) {
