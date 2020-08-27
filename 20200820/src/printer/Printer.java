df1a3e974f8222d044ab067ce8fe1a9e31a9d71b
minji-ing <kwakmj9010@gmail.com>
2020-08-27 23:54:33 +0900

    55번째 프로젝트 - 프로그래머스(프린터)-수정필

--- 20200820/src/printer/Printer.java
+++ 20200820/src/printer/Printer.java
@@ -3,11 +3,18 @@ package printer;
 public class Printer {
 	public int printer(int[] priorities, int loc) {
 		int answer = 0;
+		int prioMax = priorities[loc];
+		
 		return answer;
 	}
 	
 	public static void main(String[] args) {
 		int[] priorities = {2,1,3,2};
 		int loc = 2;
+		
+		Printer printer = new Printer();
+		int answer = printer.printer(priorities, loc);
+		
+		System.out.println(answer);
 	}
 }
