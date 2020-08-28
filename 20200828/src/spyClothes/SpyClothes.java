5d08e4f516d5b082b8af509fe2f84e24a65f6425
minji-ing <kwakmj9010@gmail.com>
2020-08-28 23:47:19 +0900

    56번째 프로젝트 - 프로그래머스(위장)-수정필

---
+++ 20200828/src/spyClothes/SpyClothes.java
@@ -0,0 +1,11 @@
+package spyClothes;
+// 프로그래머스 위장
+public class SpyClothes {
+	public int make(String[][] clothes) {
+		int answer = 0;
+		return answer;
+	}
+	public static void main(String[] args) {
+		String[][] clothes = {{"yellow-hat", "headgear"}, {"blue-sunglasses", "eyewear"}};
+	}
+}
