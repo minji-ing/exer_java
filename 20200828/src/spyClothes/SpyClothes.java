8295ad923e7e14c2a18ccc42ec50ed01a7d6cb90
minji-ing <kwakmj9010@gmail.com>
2020-08-30 00:25:46 +0900

    56번째 프로젝트 - 프로그래머스(위장)-수정필

--- 20200828/src/spyClothes/SpyClothes.java
+++ 20200828/src/spyClothes/SpyClothes.java
@@ -1,9 +1,13 @@
 package spyClothes;
+
+import java.util.ArrayList;
+import java.util.List;
+
 // 프로그래머스 위장
 public class SpyClothes {
 	public int make(String[][] clothes) {
 		int answer = 0;
-		
+		List<String> clothKind = new ArrayList<String>();
 		for(int i = 0; i < clothes.length; i++) {
 			for(int j = 0; j < clothes[i].length; j++) {
 				System.out.print(clothes[i][j] + "\t");
