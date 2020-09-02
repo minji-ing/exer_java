d6c49e10c7959db73e0ef151a0a98dee6548fca4
minji-ing <kwakmj9010@gmail.com>
2020-09-03 01:32:32 +0900

    56번째 프로젝트 - 프로그래머스(위장)-수정필

--- 20200828/src/spyClothes/SpyClothes.java
+++ 20200828/src/spyClothes/SpyClothes.java
@@ -7,26 +7,22 @@ import java.util.List;
 public class SpyClothes {
 	public int make(String[][] clothes) {
 		int answer = 0;
-		List<String> clotheKind = new ArrayList<String>();
-
-		clotheKind.add(clothes[0][2]);
+		int kinds = 1;
+		List<Integer> idx = new ArrayList<Integer>();
 		
-		for(int i = 1; i < clothes.length; i++) {
-			for(int j = 0; j < clotheKind.size(); j++) {
-				if(clothes[i][2] != clotheKind.get(j)) {
-					clotheKind.add(clothes[i][2]);
-				}
+		for(int i = 1; i < clothes[0].length; i++) {
+			if(clothes[0][0] != clothes[0][i]) {
+				kinds++;
+				idx.add(i);
 			}
 		}
-		
-		for(int i = 0; i < clotheKind.size(); i++) {
-			System.out.println(clotheKind.get(i));
-		}
-		return answer;
+		System.out.println(idx.toString());
+		return kinds;
 	}
 	public static void main(String[] args) {
 		String[][] clothes = {{"yellow-hat", "headgear"}, {"blue-sunglasses", "eyewear"}, {"green-turban", "headgear"}};
 		SpyClothes spy = new SpyClothes();
-		spy.make(clothes);
+		int answer = spy.make(clothes);
+		System.out.println(answer);
 	}
 }
