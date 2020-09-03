eb7b4ab2248be7b4815320e061d9c8162dbf4364
minji-ing <kwakmj9010@gmail.com>
2020-09-04 01:50:16 +0900

    56번째 프로젝트 - 프로그래머스(위장)-수정필

--- 20200828/src/spyClothes/SpyClothes.java
+++ 20200828/src/spyClothes/SpyClothes.java
@@ -1,23 +1,21 @@
 package spyClothes;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 // 프로그래머스 위장
 public class SpyClothes {
 	public int make(String[][] clothes) {
 		int answer = 0;
-		int kinds = 1;
-		List<Integer> idx = new ArrayList<Integer>();
+		Map<Integer, String> kinds = new HashMap<Integer, String>();
 		
-		for(int i = 1; i < clothes[0].length; i++) {
-			if(clothes[0][0] != clothes[0][i]) {
-				kinds++;
-				idx.add(i);
+		for(int i = 0; i < clothes[0].length; i++) {
+			if(kinds.containsValue(clothes[i][1]) == false) {
+				kinds.put(i, clothes[i][1]);
 			}
 		}
-		System.out.println(idx.toString());
-		return kinds;
+		System.out.println(kinds.toString());
+		return answer;
 	}
 	public static void main(String[] args) {
 		String[][] clothes = {{"yellow-hat", "headgear"}, {"blue-sunglasses", "eyewear"}, {"green-turban", "headgear"}};
